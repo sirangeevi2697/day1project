2c68c1912f5bd832bc46eaa00d86a8d13bfaa4a7
Nirmal Neethipathi <sirangeevisa97@gmail.com>
2022-11-07 09:55:42 +0530

    project

---
+++ src/test/java/sample/org/Sample.java
@@ -0,0 +1,30 @@
+package sample.org;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+
+	@Test
+	private void test1() {
+		System.out.println("Test1");
+        System.out.println("Test2"); 
+	    System.out.println("Test3");
+	    System.out.println("Test4");
+	    
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
